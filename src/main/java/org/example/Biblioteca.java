345c107967d24979b0fab2a650f0120bb3689cdd
Alex Sáez <135442605+Avzix@users.noreply.github.com>
2023-09-15 20:06:18 -0300

    Update Biblioteca.java
    Versión final del código para avance 1

--- src/main/java/org/example/Biblioteca.java
+++ src/main/java/org/example/Biblioteca.java
@@ -7,28 +7,24 @@ import java.util.Scanner;
 
 public class Biblioteca {
 
-    private static final String LIBROS_TXT = "C:\\Users\\Usuario\\IdeaProjects\\Pide_tu_libro\\src\\main\\java\\org\\example\\libros.txt";
+    private static final String LIBROS_TXT = "C:\\Users\\alexs\\IdeaProjects\\PideTuLibre\\src\\main\\java\\org\\example\\libros.txt";
+    private static List<String[]> listaLibros;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Carga la lista de libros desde el archivo de texto
-        List<String[]> listaLibros = cargarLibrosDesdeTXT(LIBROS_TXT);
+        listaLibros = cargarLibrosDesdeTXT(LIBROS_TXT);
 
         while (true) {
-            System.out.println("=== Menú de Biblioteca ===");
-            System.out.println("1. Ver libros disponibles");
-            System.out.println("2. Reservar un libro");
-            System.out.println("3. Salir");
+            mostrarMenuBiblioteca();
             int opcion = scanner.nextInt();
             scanner.nextLine();
 
             switch (opcion) {
                 case 1:
-                    mostrarLibrosDisponibles(listaLibros);
+                    mostrarMenuLibrosDisponibles(scanner);
                     break;
                 case 2:
-                    reservarLibro(scanner, listaLibros);
+                    reservarLibro(scanner);
                     break;
                 case 3:
                     System.out.println("Saliendo del programa.");
@@ -41,19 +37,26 @@ public class Biblioteca {
         }
     }
 
+    public static void mostrarMenuBiblioteca() {
+        System.out.println("|------- Menú de Biblioteca -------|");
+        System.out.println("|                                  |");
+        System.out.println("| 1) Ver libros disponibles        |");
+        System.out.println("| 2) Reservar libro                |");
+        System.out.println("| 3) Salir                         |");
+        System.out.println("|----------------------------------|");
+    }
+
     private static List<String[]> cargarLibrosDesdeTXT(String archivo) {
         List<String[]> listaLibros = new ArrayList<>();
 
-        // Verifica si el archivo existe
         File archivoLibros = new File(archivo);
 
         if (archivoLibros.exists()) {
-            // Si el archivo existe, carga los libros desde él
             try (BufferedReader reader = new BufferedReader(new FileReader(archivo))) {
                 String linea;
                 while ((linea = reader.readLine()) != null) {
                     String[] partes = linea.split(",");
-                    if (partes.length == 3) {
+                    if (partes.length == 4) {
                         listaLibros.add(partes);
                     }
                 }
@@ -67,33 +70,203 @@ public class Biblioteca {
         return listaLibros;
     }
 
-    private static void mostrarLibrosDisponibles(List<String[]> listaLibros) {
-        System.out.println("=== Libros Disponibles ===");
-        for (int i = 0; i < listaLibros.size(); i++) {
-            String[] libro = listaLibros.get(i);
-            if (Boolean.parseBoolean(libro[2])) {
-                System.out.println((i + 1) + ". " + libro[0] + " - " + libro[1]);
+    private static void mostrarMenuLibrosDisponibles(Scanner scanner) {
+        while (true) {
+            System.out.println("|----- Ver Libros Disponibles -----|");
+            System.out.println("|                                  |");
+            System.out.println("| 1. Ver libros disponibles        |");
+            System.out.println("| 2. Buscar libros por título      |");
+            System.out.println("| 3. Volver al menú principal      |");
+            System.out.println("|----------------------------------|");
+
+            int opcion = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    mostrarLibrosDisponibles();
+                    break;
+                case 2:
+                    buscarLibrosPorTitulo(scanner);
+                    break;
+                case 3:
+                    return;
+                default:
+                    System.out.println("Opción no válida. Intente nuevamente.");
+                    break;
             }
         }
     }
 
-    private static void reservarLibro(Scanner scanner, List<String[]> listaLibros) {
-        mostrarLibrosDisponibles(listaLibros);
-        System.out.print("Seleccione el número de libro que desea reservar: ");
+    private static void mostrarLibrosDisponibles() {
+        System.out.println("|--- Libros Disponibles ---|");
+        validarLibrosDisponibles();
+    }
+
+    private static void validarLibrosDisponibles() {
+        for (int i = 0; i < listaLibros.size(); i++) {
+            String[] libro = listaLibros.get(i);
+            String titulo = libro[0];
+            String autor = libro[1];
+            String isbn = libro[2];
+            int copiasDisponibles = Integer.parseInt(libro[3]);
+
+            System.out.println((i + 1) + ". " + titulo + " - " + autor + " (ISBN: " + isbn + ")");
+
+            if (copiasDisponibles == 0) {
+                System.out.println("No quedan copias de este libro.");
+            } else {
+                System.out.println("Copias disponibles: " + copiasDisponibles);
+            }
+        }
+    }
+
+    private static void buscarLibrosPorTitulo(Scanner scanner) {
+        System.out.print("Ingrese el título del libro que desea buscar: ");
+        String tituloBuscado = scanner.nextLine();
+        List<String[]> librosEncontrados = new ArrayList<>();
+
+        for (String[] libro : listaLibros) {
+            if (libro[0].equalsIgnoreCase(tituloBuscado) && Integer.parseInt(libro[3]) > 0) {
+                librosEncontrados.add(libro);
+            }
+        }
+
+        if (!librosEncontrados.isEmpty()) {
+            System.out.println("|--- Libros Encontrados ---|");
+            for (int i = 0; i < librosEncontrados.size(); i++) {
+                String[] libro = librosEncontrados.get(i);
+                String titulo = libro[0];
+                String autor = libro[1];
+                String isbn = libro[2];
+                int copiasDisponibles = Integer.parseInt(libro[3]);
+
+                System.out.println((i + 1) + ". " + titulo + " - " + autor + " (ISBN: " + isbn + ")");
+                System.out.println("Copias disponibles: " + copiasDisponibles);
+            }
+        } else {
+            System.out.println("No se encontró ningún libro disponible con ese título o no quedan copias.");
+        }
+    }
+
+    private static void reservarLibro(Scanner scanner) {
+        System.out.println("|--- Reservar Libro ---|");
+        System.out.println("Seleccione cómo desea buscar el libro:");
+        System.out.println("1. Buscar por Título");
+        System.out.println("2. Buscar por ISBN");
+        System.out.println("3. Volver al menú principal");
+
         int opcion = scanner.nextInt();
         scanner.nextLine();
 
-        if (opcion >= 1 && opcion <= listaLibros.size()) {
-            String[] libroSeleccionado = listaLibros.get(opcion - 1);
-            if (Boolean.parseBoolean(libroSeleccionado[2])) {
+        switch (opcion) {
+            case 1:
+                buscarLibroPorTituloYReservar(scanner);
+                break;
+            case 2:
+                buscarLibroPorISBNYReservar(scanner);
+                break;
+            case 3:
+                return;
+            default:
+                System.out.println("Opción no válida. Intente nuevamente.");
+                break;
+        }
+    }
+
+    private static void buscarLibroPorTituloYReservar(Scanner scanner) {
+        System.out.print("Ingrese el título del libro que desea reservar: ");
+        String tituloBuscado = scanner.nextLine();
+        List<String[]> librosDisponibles = new ArrayList<>();
+
+        for (String[] libro : listaLibros) {
+            if (libro[0].equalsIgnoreCase(tituloBuscado) && Integer.parseInt(libro[3]) > 0) {
+                librosDisponibles.add(libro);
+            }
+        }
+
+        if (!librosDisponibles.isEmpty()) {
+            System.out.println("|--- Libros Disponibles ---|");
+            for (int i = 0; i < librosDisponibles.size(); i++) {
+                String[] libro = librosDisponibles.get(i);
+                String titulo = libro[0];
+                String autor = libro[1];
+                String isbn = libro[2];
+                int copiasDisponibles = Integer.parseInt(libro[3]);
+
+                System.out.println((i + 1) + ". " + titulo + " - " + autor + " (ISBN: " + isbn + ")");
+                System.out.println("Copias disponibles: " + copiasDisponibles);
+            }
+
+            System.out.print("Seleccione el número de libro que desea reservar o ingrese 0 para volver: ");
+            int seleccion = scanner.nextInt();
+            scanner.nextLine();
+
+            if (seleccion >= 1 && seleccion <= librosDisponibles.size()) {
+                String[] libroSeleccionado = librosDisponibles.get(seleccion - 1);
                 System.out.println("Reservando el libro: " + libroSeleccionado[0]);
-                libroSeleccionado[2] = "false"; // Cambiar el estado a no disponible
+                int copiasDisponibles = Integer.parseInt(libroSeleccionado[3]);
+                copiasDisponibles--; // Reducir la cantidad de copias disponibles en 1
+                libroSeleccionado[3] = String.valueOf(copiasDisponibles); // Actualizar la cantidad en el arreglo
+                if (copiasDisponibles == 0) {
+                    System.out.println("No quedan copias de este libro.");
+                }
                 guardarLibrosEnTXT(LIBROS_TXT, listaLibros);
+            } else if (seleccion == 0) {
+                return;
             } else {
-                System.out.println("El libro seleccionado no está disponible.");
+                System.out.println("Opción no válida. Intente nuevamente.");
             }
         } else {
-            System.out.println("Opción no válida. Intente nuevamente.");
+            System.out.println("No se encontró ningún libro disponible con ese título o no quedan copias.");
+        }
+    }
+
+    private static void buscarLibroPorISBNYReservar(Scanner scanner) {
+        System.out.print("Ingrese el ISBN del libro que desea reservar: ");
+        String isbnBuscado = scanner.nextLine();
+        List<String[]> librosDisponibles = new ArrayList<>();
+
+        for (String[] libro : listaLibros) {
+            if (libro[2].equalsIgnoreCase(isbnBuscado) && Integer.parseInt(libro[3]) > 0) {
+                librosDisponibles.add(libro);
+            }
+        }
+
+        if (!librosDisponibles.isEmpty()) {
+            System.out.println("|--- Libros Disponibles ---|");
+            for (int i = 0; i < librosDisponibles.size(); i++) {
+                String[] libro = librosDisponibles.get(i);
+                String titulo = libro[0];
+                String autor = libro[1];
+                String isbn = libro[2];
+                int copiasDisponibles = Integer.parseInt(libro[3]);
+
+                System.out.println((i + 1) + ". " + titulo + " - " + autor + " (ISBN: " + isbn + ")");
+                System.out.println("Copias disponibles: " + copiasDisponibles);
+            }
+
+            System.out.print("Seleccione el número de libro que desea reservar o ingrese 0 para volver: ");
+            int seleccion = scanner.nextInt();
+            scanner.nextLine();
+
+            if (seleccion >= 1 && seleccion <= librosDisponibles.size()) {
+                String[] libroSeleccionado = librosDisponibles.get(seleccion - 1);
+                System.out.println("Reservando el libro: " + libroSeleccionado[0]);
+                int copiasDisponibles = Integer.parseInt(libroSeleccionado[3]);
+                copiasDisponibles--; // Reducir la cantidad de copias disponibles en 1
+                libroSeleccionado[3] = String.valueOf(copiasDisponibles); // Actualizar la cantidad en el arreglo
+                if (copiasDisponibles == 0) {
+                    System.out.println("No quedan copias de este libro.");
+                }
+                guardarLibrosEnTXT(LIBROS_TXT, listaLibros);
+            } else if (seleccion == 0) {
+                return;
+            } else {
+                System.out.println("Opción no válida. Intente nuevamente.");
+            }
+        } else {
+            System.out.println("No se encontró ningún libro disponible con ese ISBN o no quedan copias.");
         }
     }
 
